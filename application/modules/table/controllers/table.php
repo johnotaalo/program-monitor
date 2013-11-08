1bf3d4f2c599508974d7d7355a04621501a40d82
KevinMarete55443 <kevomarete@gmail.com>
2013-11-08 16:40:17 +0300

    anti-login

--- application/modules/table/controllers/table.php
+++ application/modules/table/controllers/table.php
@@ -11,10 +11,10 @@ class Table extends MY_Controller {
 	public function load_table($columns = array(), $table_data = array(), $set_options = 1) {
 		$this -> load -> library('table');
 		array_unshift($columns, "#");
-		$tmpl = array('table_open' => '<table id="dyn_table" class="table table-striped table-bordered table-condensed">');
+		$tmpl = array('table_open' => '<table id="dyn_table" class="table table-striped table-bordered table-condensed dataTables">');
 		$this -> table -> set_template($tmpl);
 		$this -> table -> set_heading($columns);
-		$options = "<a href='#'>View</a>";
+		$options = '<button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#view">View</button>';
 		$counter = 1;
 
 		foreach ($table_data as $table) {
--- application/modules/template/controllers/template.php
+++ application/modules/template/controllers/template.php
@@ -10,13 +10,13 @@ class Template extends MY_Controller {
 
 	public function index($data) {
 		$user_session = $this -> check_session();
-		if($user_session){
-		$data['banner_title'] = $this -> config -> item('banner_title');
-		$data['banner_subtitle'] = $this -> config -> item('banner_subtitle');
-		$data['firm_name'] = $this -> config -> item('firm_name');
-		$data['default_home_controller'] = $this -> config -> item('default_home_controller');
-		$this -> load -> view('template_v', $data);
-		}else{
+		if ($user_session) {
+			$data['banner_title'] = $this -> config -> item('banner_title');
+			$data['banner_subtitle'] = $this -> config -> item('banner_subtitle');
+			$data['firm_name'] = $this -> config -> item('firm_name');
+			$data['default_home_controller'] = $this -> config -> item('default_home_controller');
+			$this -> load -> view('template_v', $data);
+		} else {
 			redirect("login");
 		}
 	}
@@ -25,6 +25,8 @@ class Template extends MY_Controller {
 		$current_url = $this -> router -> class;
 		if ($current_url != "login" && $this -> session -> userdata("id") == null) {
 			return false;
+		} else if ($current_url == "login" && $this -> session -> userdata("id") != null) {
+			redirect($this -> config -> item('module_after_login'));
 		} else {
 			return true;
 		}
