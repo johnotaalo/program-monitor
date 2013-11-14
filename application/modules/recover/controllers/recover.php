9c360b8d3a09987179e91c09d03adb4fcce16a29
KevinMarete55443 <kevomarete@gmail.com>
2013-11-14 18:00:01 +0300

    recover

--- application/modules/recover/controllers/recover.php
+++ application/modules/recover/controllers/recover.php
@@ -3,7 +3,7 @@ if (!defined('BASEPATH'))
 	exit('No direct script access allowed');
 
 class Recover extends MY_Controller {
-
+	var $backup_dir = "./backup_db";
 	function __construct() {
 		parent::__construct();
 	}
@@ -16,8 +16,7 @@ class Recover extends MY_Controller {
 	}
 
 	public function checkdir() {
-		$back_folder = "backup_db";
-		$dir = './' . $back_folder;
+		$dir = $this -> backup_dir;
 		$backup_files = array();
 		$backup_headings = array('Filename', 'Options');
 		$options = '<button class="btn btn-primary btn-sm recover" >Recover</button>';
@@ -38,7 +37,32 @@ class Recover extends MY_Controller {
 
 	public function start_recovery() {
 		$file_name = $this -> input -> post("file_name", TRUE);
-		return $file_name;
+		$dir = $this -> backup_dir;
+		$file_path = realpath($dir . "/" . $file_name);
+		//$file_path=$this->uncompress_zip($file_path);
+		$file_path="\"".$file_path."\"";
+
+		$CI = &get_instance();
+		$CI -> load -> database();
+		$hostname = $CI -> db -> hostname;
+		$username = $CI -> db -> username;
+		$password = $CI -> db -> password;
+		$current_db = $CI -> db -> database;
+		$recovery_status = false;
+
+		$this -> load -> dbutil();
+		if ($this -> dbutil -> database_exists($current_db)) {
+			$recovery_status = true;
+			$command="cd ".$_SERVER['DOCUMENT_ROOT'];
+			$command=str_replace("htdocs", "mysql/bin", $command);
+			//exec($command);
+			echo $command ='mysql -u ' . $username . ' -p ' . $password . ' ' . $current_db . ' < ' . $file_path;
+			//exec($command);
+		}
+		return $recovery_status;
+	}
+	public function uncompress_zip($file_path){
+		
 	}
 
 	public function template($data) {
