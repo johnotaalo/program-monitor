99a0a83327eb51a5c418c84e09070733e9935df2
CHRISPINE OTAALO <john.otaalo@strathmore.edu>
2014-06-20 12:30:29 +0300

    Database changes

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
-$db['default']['database'] = 'program-monitor';
+$db['default']['password'] = 'Chris';
+$db['default']['database'] = 'program_monitor';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
